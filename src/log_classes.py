068b15af26a25b4aa5fe878e6576faf000d581f9
Madhavan Sriram <maddy2u@gmail.com>
2021-08-12 12:30:49 +0200

    Updated the logging information

--- src/log_classes.py
+++ src/log_classes.py
@@ -8,17 +8,16 @@ logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:
 class SpecialChar:
     def __init__(self, symbol):
         self.symbol = symbol
-        logging.critical(f'{symbol} has been initialized')
+        logging.info(f'{symbol} has been initialized')
 
     def __repr__(self):
-        logging.critical('Inside the __repr__ function')
+        logging.info('Inside the __repr__ function')
         return 'Special Character {%r}' % self.symbol
 
     def convertordinal(self):
-        logging.critical('Inside the convertordinal function')
+        logging.info('Inside the convertordinal function')
         return ord(self.symbol)
 
 
 if __name__ == '__main__':
     print(SpecialChar('#').convertordinal())
-    # print(SpecialChar.convertordinal('$'))
