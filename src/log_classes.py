fa40fa82e06309a4aa21d0f68584f0ec46130c63
Madhavan Sriram <maddy2u@gmail.com>
2021-08-12 14:12:17 +0200

    Updated the logging information

--- src/log_classes.py
+++ src/log_classes.py
@@ -2,22 +2,27 @@ import logging
 import pytest
 
 # General Guidelines for logging
-logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+log_handler = logging.FileHandler('file.log')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 
 class SpecialChar:
-    def __init__(self, symbol):
+    def __init__(self, symbol:str) -> None:
         self.symbol = symbol
         logging.info(f'{symbol} has been initialized')
 
-    def __repr__(self):
-        logging.info('Inside the __repr__ function')
+    def __repr__(self) -> str:
+        logging.info(f'Inside the {__name__} function')
         return 'Special Character {%r}' % self.symbol
 
-    def convertordinal(self):
-        logging.info('Inside the convertordinal function')
+    def convert_ordinal(self) -> int:
+        logging.info(f'Inside the {__name__} function')
         return ord(self.symbol)
 
 
+def instance():
+    return SpecialChar('#').convert_ordinal()
+
+
 if __name__ == '__main__':
-    print(SpecialChar('#').convertordinal())
+    print(instance())
