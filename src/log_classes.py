354cc14cdd93ba3152e49a2a33466ebc0084fbe9
Madhavan Sriram <maddy2u@gmail.com>
2021-08-12 16:20:41 +0200

    Update log_classes.py

--- src/log_classes.py
+++ src/log_classes.py
@@ -6,7 +6,6 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 
-
 class SpecialChar:
     def __init__(self, symbol: str) -> None:
         self.symbol = symbol
